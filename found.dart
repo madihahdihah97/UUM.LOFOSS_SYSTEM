66b96f277a06bc4c8a8db2dbfa33300600b4590e
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2021-02-06 23:28:29 +0800

    Create found.dart

---
+++ found.dart
@@ -0,0 +1,6 @@
+class Found{
+  final String typeitem,colorOrModel,wherefound,datefound,foundby,deliverymethod;
+  Found({
+     this.typeitem,this.colorOrModel,this.wherefound,this.datefound,this.foundby,this.deliverymethod,
+  });
+}
